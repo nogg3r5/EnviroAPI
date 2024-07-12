e0af782510cdfa4903b91ebab600a1f10fe978c6
Shaun <nogg3r5@gmail.com>
2024-07-12 15:54:30 +0100

    Ability to wuery api end point for averages

--- enviroapi.py
+++ enviroapi.py
@@ -38,6 +38,30 @@ def latest():
  sqliteConnection.close()
  return flask.jsonify(data)
 
+#http://192.168.4.48:5000/average?days=3&time=h or d or m
+#select AVG(temp),datetime from Enviro where datetime>= datetime('now','-180 day','localtime') group by strftime('%YYYY-%mmm-%dd %HH', datetime);
+@app.route('/average', methods= ['GET'])
+def average():
+ days = flask.request.args.get('days', default=180, type = float)
+ days = "'-"+str(days)+" day'"
+ time = flask.request.args.get('time', default='d')
+ if time == 'd':
+  time = '%YYYY-%mmm-%dd'
+ elif time == 'h':
+  time = '%YYYY-%mmm-%dd %HH'
+ elif time == 'm':
+  time = '%YYYY-%mmm'
+ else:
+  time = '%YYYY-%mmm-%dd'
+ sqliteConnection = sqlite3.connect('/home/pi/database/enviro.db')
+ query = "select AVG(temp),datetime from Enviro where datetime>= datetime('now',"+days+",'localtime') group by strftime('"+time+"', datetime);"
+ cursor = sqliteConnection.cursor()
+ cursor.execute(query)
+ data = cursor.fetchall()
+ cursor.close()
+ sqliteConnection.close()
+ return flask.jsonify(data)
+
 @app.route('/upload')
 def my_form():
     return flask.render_template('upload.html')
