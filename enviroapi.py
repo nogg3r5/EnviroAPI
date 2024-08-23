535ae4668232f319820659338cd7dae2ddfd8b2c
Shaun <nogg3r5@gmail.com>
2024-08-23 06:30:09 +0100

    Removing uploader

--- enviroapi.py
+++ enviroapi.py
@@ -38,6 +38,19 @@ def custom():
  sqliteConnection.close()
  return flask.jsonify(data)
 
+@app.route('/custompidata', methods=['GET'])
+def custompidata():
+ days = flask.request.args.get('days', default=1, type = float)
+ days = "'-"+str(days)+" day'"
+ sqliteConnection = sqlite3.connect('/home/pi/database/pidata.db')
+ query = "SELECT * from pidata where datetime>= datetime('now',"+days+");"
+ cursor = sqliteConnection.cursor()
+ cursor.execute(query)
+ data = cursor.fetchall()
+ cursor.close()
+ sqliteConnection.close()
+ return flask.jsonify(data)
+
 @app.route('/latest', methods= ['GET'])
 def latest():
  sqliteConnection = sqlite3.connect('/home/pi/database/enviro.db')
@@ -72,27 +85,5 @@ def average():
  cursor.close()
  sqliteConnection.close()
  return flask.jsonify(data)
-
-@app.route('/upload')
-def my_form():
-    return flask.render_template('upload.html')
-
-@app.route('/upload', methods=['POST'])
-def my_form_post():
-    text = flask.request.form['text']
-    type = flask.request.form['type']
-    print(text)
-    print(type)
-    with open("links.txt", "a") as fo:
-     fo.write(text+","+type+"\n")
-    return flask.render_template('upload.html')
-
-#http://192.168.4.48:5000/uploadurl/https://google.co.uk
-@app.route('/uploadurl/<path:input_url>')
-def UploadURL(input_url):
-    url = input_url
-    with open("links.txt", "a") as fo:
-     fo.write(url +"\n")
-    return url
 
 app.run(host='0.0.0.0')
